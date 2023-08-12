f89530e2a65cb49798e6958a86b9cc26e0ffeef3
Japneet Rajput <82120186+JapneetRajput@users.noreply.github.com>
2023-08-12 14:43:56 +0530

    Create mimikatz.bat

---
+++ mimikatz.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+
+rem Download from https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20220919/mimikatz_trunk.zip
+
+rem Extract it 
+
+rem Change the working directory to where mimikatz is located
+cd mimikatz_trunk\x64
+
+rem Run mimikatz.exe
+mimikatz.exe
+
+rem Pause to keep the console window open after mimikatz is done
+pause
